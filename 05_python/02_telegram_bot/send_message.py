c20495ce152bcaa0829356795bd00260fa6db198
kyhoon001 <kyhoon001@naver.com>
2019-12-13 11:47:14 +0900

    191213 | added send_message.py

---
+++ 05_python/02_telegram_bot/send_message.py
@@ -0,0 +1,14 @@
+import requests
+from decouple import config
+
+
+base = 'https://api.telegram.org'
+token = config('TELEGRAM_BOT_TOKEN')
+chat_id = config('CHAT_ID')
+text = 'hello'
+
+url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+send_message = requests.get(url)
+
+
+print(send_message)
