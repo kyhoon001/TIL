6552f852de9ec1b5ec2a8d8b7087e390eab7ec52
kyhoon001 <kyhoon001@naver.com>
2019-12-13 15:08:41 +0900

    191213 | telegram_setWebhook

--- 05_python/02_telegram_bot/app.py
+++ 05_python/02_telegram_bot/app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from decouple import config
+import requests
 app = Flask(__name__)
+
+
+base = 'https://api.telegram.org'
+token = config('TELEGRAM_BOT_TOKEN')
+chat_id = config('CHAT_ID')
+text = 'hello'
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/write')
+def write():
+    return render_template('write.html')
 
+@app.route('/send')
+def send():
+    text = request.args.get('message')
 
+    requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+    return render_template('send.html')
 
+@app.route(f'/{token}', methods=['POST'])
+def telegram():
+    print(request.get_json())
+    from_telegram = request.get_json()
+
+    if from_telegram.get('message') is not None:
+        chat_id = from_telegram.get('message').get('from').get('id')
+        text = from_telegram('message').get('text')
+        request.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+        requests.get(url)
+    return '',200
