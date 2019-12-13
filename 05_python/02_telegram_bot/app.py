c9adfb95cc40bcb581d89f08c3151f94329d2b03
kyhoon001 <kyhoon001@naver.com>
2019-12-13 17:15:39 +0900

    update app.py(translate)

--- 05_python/02_telegram_bot/app.py
+++ 05_python/02_telegram_bot/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template,request
 from decouple import config
 import requests
+from IPython import embed
+
 app = Flask(__name__)
 
 
 base = 'https://api.telegram.org'
 token = config('TELEGRAM_BOT_TOKEN')
 chat_id = config('CHAT_ID')
-text = 'hello'
+naver_client_id = config('NAVER_CLIENT_ID')
+naver_client_secret = config('NAVER_CLIENT_SECRET')
 
 
 @app.route('/')
@@ -25,14 +28,32 @@ def send():
     requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return render_template('send.html')
 
+
+#왜 안 되냐 근데 진짜 짜증나게
 @app.route(f'/{token}', methods=['POST'])
 def telegram():
-    print(request.get_json())
+    # print(request.get_json())
     from_telegram = request.get_json()
-
+    # embed()
     if from_telegram.get('message') is not None:
         chat_id = from_telegram.get('message').get('from').get('id')
-        text = from_telegram('message').get('text')
-        request.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
-        requests.get(url)
-    return '',200
+        text = from_telegram.get('message').get('text')
+        url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+        # requests.get(url)
+
+
+
+        if text[0:2] =='/ ':
+            
+            headers = {
+                'X-Naver-Client-Id':naver_client_id,
+                'X-Naver-Client-Secret':naver_client_secret
+            }
+            data = {'source':'ko', 'target':'en', 'text':text[2:]}
+            papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt',headers=headers,data=data)
+
+
+            text=papago_res.json().get('message').get('result').get('translatedText')
+
+            requests.get(f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+        return '',200
