24ad3179410968c40528733fe3789df5cba85363
kyhoon001 <kyhoon001@naver.com>
2020-02-23 15:20:10 +0900

    경로찾기 bfs로 수정

--- 알고리즘공부/python문법연습/백준-11403-경로찾기.py
+++ 알고리즘공부/python문법연습/백준-11403-경로찾기.py
@@ -1,33 +1,35 @@
 n = int(input())
+
+q = []
 arr = []
-check = []
-def solve(start, a) :
-		
+
+
+def bfs(start) :
+	
+
+	check = [0] * n
+	while q :
+		a = q.pop(0)
 		for i in range(n) :
-			if arr[a][i] == 1 : #간선이 있다면
-			
-				if check[a][i] == False : #만약 탐색이 안 되었으면
-					check[a][i] = True #탐색을 하고
-					arr[start][i] =1 # 처음 시작한 데서 목적지까지  경로가 있음
-					
-					solve(start,i)
-												
+			if arr[a][i] == 1 and check[i] == 0:
+				q.append(i)
+				arr[start][i] = 1
+				check[i] =1
+	return
 
-for i in range(n) :
+for _ in range(n):
 	arr.append(list(map(int,input().split())))
-	check.append([False] * n)
+		
+		
 
+#print(arr[2][2])
 for i in range(n) :
-	
-	solve(i,i)
-	for j in range(n) :
-		for t in range(n) :
-			check[j][t] = False
-	
-for i in range(n) :	
-	for j in range(n) :
-		print(arr[i][j], end=" ")
-	print()	
+	q.append(i)
+	bfs(i)
 
+
+for i in range(n):
+	for j in range(n):
+		print(arr[i][j], end= " ")
+	print()
 	
-				
